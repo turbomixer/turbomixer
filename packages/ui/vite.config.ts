9d03639957e963434a28ca76bdb8fa02f9b12522
TimeBather <open-source@timebather.cn>
2023-12-03 16:59:13 +0800

    refactor(ui): move library build config from yakumo vite to ui library

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        emptyOutDir: true,
+        cssCodeSplit: false,
+        lib: {
+            entry: path.resolve(directory, 'src/index.ts'),
+            formats: ['es']
+        },
+        outDir: 'lib/'
+    },
     plugins: [vue()],
 })
--- yakumo/vite.ts
+++ yakumo/vite.ts
@@ -7,16 +7,7 @@ register('vite',(project)=>{
         const directory = path.join(project.cwd , dir);
 
         buildVite({
-            root:directory,
-            build:{
-                emptyOutDir: true,
-                cssCodeSplit: false,
-                lib:{
-                    entry:path.resolve(directory,'src/index.ts'),
-                    formats:['es']
-                },
-                outDir:'lib/'
-            }
+            root:directory
         })
 
     })
