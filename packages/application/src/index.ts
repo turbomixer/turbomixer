e304a4aa7e8ad8270a0f4dc6af703cea5d872b1b
TimeBather <open-source@timebather.cn>
2023-12-06 17:11:10 +0800

    feat(application):add module loader

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -2,12 +2,14 @@ import {createTurbomixer} from "@turbomixer/core";
 import {UiService} from "@turbomixer/ui";
 import {TurboMixerOfficialClient} from "@turbomixer/client";
 import "@turbomixer/ui/lib/style.css";
+import {TurboMixerModuleLoader} from "@turbomixer/loader";
 
 declare global{
     interface Window{
         config? : {
             server ? : string
-        }
+        },
+        turbomixer:any
     }
 }
 
@@ -15,6 +17,10 @@ declare global{
 
 const turbomixer = createTurbomixer();
 
+window.turbomixer = turbomixer
+
 turbomixer.plugin(UiService,{element:document.getElementById("app")});
 
 const plugin = turbomixer.plugin(TurboMixerOfficialClient,{server: window.config?.server ?? '/api'});
+
+turbomixer.plugin(TurboMixerModuleLoader)
