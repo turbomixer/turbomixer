7744561e95f6a79891b7ec2088879b5f56e9783b
TimeBather <open-source@timebather.cn>
2023-12-21 21:38:58 +0800

    feat(ui): ProjectNew dialog update

--- packages/ui/src/dialog/project/ProjectNew.vue
+++ packages/ui/src/dialog/project/ProjectNew.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {computed} from "vue";
+import {computed, ref} from "vue";
 import Dialog from "../Dialog.vue";
 
 const props = defineProps(['title','id','modelValue'])
@@ -12,6 +12,12 @@ const model = computed({
     emit('update:modelValue', value)
   }
 })
+
+const wizard_step = ref(0);
+
+const runtime = ref('');
+
+const entry = ref('')
 </script>
 
 <template>
@@ -22,25 +28,59 @@ const model = computed({
           新建一个TurboMixer项目
         </span>
       </div>
-      <div style="display: flex;flex-direction: column;flex:1">
+      <div style="display: flex;flex-direction: column;flex:1" v-if="wizard_step == 0">
         <div style="padding: 1em;flex:1">
           插件名称:<input style="width: 20em" value="未命名插件_cbfe98cd"/><br/>
           保存位置:<select style="width: 20em"><option>Koishi服务器(172.**.**.24)</option></select><br/>
-          输出语言:<select style="width: 20em"><option>JavaScript</option></select><br/>
-          运行目标:<select style="width: 20em"><option>Koishi</option></select><br/>
         </div>
         <div style="height: 1.5em;padding-top: 0.5em;border-top: 1px solid #cccccc;text-align: end;padding-right: 1em">
-          <button class="turbomixer-dialog-button">下一步</button>
+          <button class="turbomixer-dialog-button" @click="wizard_step = 1">下一步</button>
         </div>
       </div>
-      <div style="display: flex;flex-direction: column;flex:1">
-        <div style="padding: 1em;flex:1">
 
+      <div style="display: flex;flex-direction: column;flex:1" v-else-if="wizard_step == 1">
+        <div style="padding: 1em;flex:1">
+          你想运行在.....:<br/>
+          <input type="radio" value="koishi" v-model="runtime">Koishi
         </div>
         <div style="height: 1.5em;padding-top: 0.5em;border-top: 1px solid #cccccc;text-align: end;padding-right: 1em">
-          <button class="turbomixer-dialog-button">创建</button>
+          <button class="turbomixer-dialog-button" @click="wizard_step = 2">下一步</button>
         </div>
       </div>
+
+      <div style="display: flex;flex-direction: column;flex:1" v-else-if="wizard_step == 2">
+        <div style="padding: 1em;flex:1">
+          你想用.....作为你的主要编程方式:<br/>
+
+          <input type="radio" value="blockly" v-model="entry">积木编程(Blockly)<br/>
+          <input type="radio" value="flow" v-model="entry">响应式流编程
+        </div>
+        <div style="height: 1.5em;padding-top: 0.5em;border-top: 1px solid #cccccc;text-align: end;padding-right: 1em">
+          <button class="turbomixer-dialog-button" @click="wizard_step = 3">下一步</button>
+        </div>
+      </div>
+
+      <div style="display: flex;flex-direction: column;flex:1" v-else-if="wizard_step == 3">
+        <div style="padding: 1em;flex:1">
+          你想启用的插件:
+        </div>
+        <div style="height: 1.5em;padding-top: 0.5em;border-top: 1px solid #cccccc;text-align: end;padding-right: 1em">
+          <button class="turbomixer-dialog-button" @click="wizard_step = 4">下一步</button>
+        </div>
+      </div>
+
+      <div style="display: flex;flex-direction: column;flex:1" v-else-if="wizard_step == 4">
+        <div style="padding: 1em;flex:1">
+          确认配置信息:<br/>
+          运行时环境:Koishi<br/>
+          语言:TypeScript<br/>
+          入口类型:积木编程(Blockly)<br/>
+        </div>
+        <div style="height: 1.5em;padding-top: 0.5em;border-top: 1px solid #cccccc;text-align: end;padding-right: 1em">
+          <button class="turbomixer-dialog-button">完成</button>
+        </div>
+      </div>
+
     </div>
   </Dialog>
 </template>
